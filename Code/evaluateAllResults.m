6155e39562903b44bcbbe7eaad68e6ba2a609eb2
Nany12345 <nany@mail.sustc.edu.cn>
2020-04-10 17:40:45 +0800

    Version 2

--- Code/evaluateAllResults.m
+++ Code/evaluateAllResults.m
@@ -7,14 +7,13 @@ function evaluateAllResults(dimension)
     solution_number = 100;
     set_number = 100;
     max_vec = 1;
-    for WVMethod = {'UNV','JAS','Proposed'}
+    for WVMethod = {'UNV','JAS','DAS','MSS-U','MSS-D'}
         seed_number = 30;
         if strcmp(WVMethod,'DAS') || strcmp(WVMethod,'MSS-D')
             seed_number = 1;
         end
-        for problem_type = {'linear_triangular','concave_triangular','convex_triangular', ...
-                'linear_invertedtriangular','concave_invertedtriangular','convex_invertedtriangular'}
-%         for problem_type = {'linear_triangular'}
+        for problem_type = {'linear_triangular'}%,'concave_triangular','convex_triangular', ...
+               % 'linear_invertedtriangular','concave_invertedtriangular','convex_invertedtriangular'}
            for reference_point = 0:1:4
                 evaluate_result = zeros(max_vec, 2, seed_number);
                 for seed = 1:seed_number
@@ -25,10 +24,12 @@ function evaluateAllResults(dimension)
                         continue;
                     end
                     evaluate_result(max_vec,:,seed) = arr;
-                    disp([WVMethod,'_',problem_type,'_',num2str(reference_point),'_',num2str(seed)]);
+                    dis_play = strcat(WVMethod,'_',problem_type,'_',num2str(reference_point),'_',num2str(seed));
+                    disp(dis_play);
                 end   
                 file_name = strcat('./Result/evaluate_result_dim_',num2str(dimension), ...
-                    '_probtype_',problem_type,'_numVec_',num2str(num_vector),'_numSol_',num2str(solution_number), ...
+                    '_numVec_',num2str(num_vector),'_probtype_',problem_type, ...
+                    '_numSol_',num2str(solution_number), ...
                     '_',num2str(reference_point),'_',WVMethod,'.mat');
                 file_name = file_name{:};
                 save(file_name, "evaluate_result");
